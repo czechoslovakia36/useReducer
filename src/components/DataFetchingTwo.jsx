bd1ed6ef8643ce6ca366f5ff1087460bbbf7edd2
Vivek Kumar <clocktower77@protonmail.com>
2023-03-15 12:24:50 +0530

    DataFetchingTwo Component Added

---
+++ src/components/DataFetchingTwo.jsx
@@ -0,0 +1,13 @@
+import React,{useReducer,useEffect} from 'react';
+
+
+
+const DataFetchingTwo = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default DataFetchingTwo;
